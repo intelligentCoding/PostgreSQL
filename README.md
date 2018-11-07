d22d9fc5f30e6e55bf3c90a2a329f2264b888d49
intelligentCoding <31515792+intelligentCoding@users.noreply.github.com>
2018-11-06 20:12:05 -0500

    Deploying on Heroku

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-"# PostgreSQL"
+# PostgreSQL"
+
 #Things to Install</br>
 **npm install express**</br>
 **npm install body-parser**</br>
@@ -7,5 +8,6 @@ To handle HTTP POST request in Express.js version 4 and above, you need to insta
 body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.</br>
 The middleware was a part of Express.js earlier but now you have to install it separately.</br>
 This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request. Install body-parser using NPM as shown below.
-
-"
+</br>
+</br>
+**npm install --save express-handlebars**
--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 //We will be using handlebars so we are requiring the handle bars.
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
+const HTTP_PORT = process.env.PORT || 8080;
 
+//We call this function when we start our server
 
+function onHttpStart() {
+  console.log("Express http server listening on : " + HTTP_PORT);
+}
+//Instruct the app to use body-parsers middleware.
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs"
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Well Hello");
+});
+app.listen(HTTP_PORT, onHttpStart);
